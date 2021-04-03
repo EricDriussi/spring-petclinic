97767d5dce67372a7384848847eec922877e22ff
Eric Driussi <ericdriussi@gmail.com>
2021-04-03 21:15:45 +0100

    base entity with id 💪 closes #10

--- pet-clinic-data/src/main/java/udemy/spring/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/udemy/spring/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package udemy.spring.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/udemy/spring/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/udemy/spring/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package udemy.spring.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
