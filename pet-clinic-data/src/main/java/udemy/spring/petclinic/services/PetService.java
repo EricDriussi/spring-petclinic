fade62d2b10a756715d657b847d020699136cc90
Eric Driussi <ericdriussi@gmail.com>
2021-04-04 12:15:11 +0100

    😎 cool banner & #11

--- pet-clinic-data/src/main/java/udemy/spring/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/udemy/spring/petclinic/services/PetService.java
@@ -3,15 +3,6 @@ package udemy.spring.petclinic.services;
 
 import udemy.spring.petclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-
-    Pet findById(Long id);
-
-    Pet save (Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long>{
 
 }
--- pet-clinic-data/src/main/java/udemy/spring/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/udemy/spring/petclinic/services/VetService.java
@@ -2,14 +2,6 @@ package udemy.spring.petclinic.services;
 
 import udemy.spring.petclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save (Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 
 }
