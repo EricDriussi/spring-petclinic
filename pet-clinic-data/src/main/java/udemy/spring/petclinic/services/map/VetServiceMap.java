a17d565d154dd8e214e990616efb26bd4e513d0c
Eric Driussi <ericdriussi@gmail.com>
2021-04-04 12:40:16 +0100

    map services 👻 closes #6

---
+++ pet-clinic-data/src/main/java/udemy/spring/petclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package udemy.spring.petclinic.services.map;
+
+import udemy.spring.petclinic.model.Vet;
+import udemy.spring.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet,Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        super.delete(vet);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return save(vet.getId(), vet);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteByID(id);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
