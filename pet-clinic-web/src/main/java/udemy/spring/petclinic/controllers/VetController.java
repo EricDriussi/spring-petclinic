d0aa6c17c7349c3d0e50d251ccaf4582e9ed853b
Eric Driussi <ericdriussi@gmail.com>
2021-04-04 17:54:52 +0100

    vets list 🐱 closes #15

--- pet-clinic-web/src/main/java/udemy/spring/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/udemy/spring/petclinic/controllers/VetController.java
@@ -1,13 +1,25 @@
 package udemy.spring.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import udemy.spring.petclinic.services.VetService;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets", "/vets/vet-list", "/vets/vet-list.html"})
-    public String listVets() {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"", "/", "/vet-list", "/vet-list.html"})
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/vet-list";
     }
 }
