ac36b70fe4712399f02f2789faba6da557ea4c5a
Eric Driussi <ericdriussi@gmail.com>
2021-04-04 15:42:16 +0100

    owner-controller 🍃 closes #13

---
+++ pet-clinic-web/src/main/java/udemy/spring/petclinic/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package udemy.spring.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "/", "/owner-list", "/owner-list.html"})
+    public String listOwners(){
+        return "owners/owner-list";
+    }
+}
